da27e28d26cd82513a47df18e0caa6dd4613f1da
Yoshie2000 <patrick.leonhardt@gmx.net>
2026-02-15 18:11:07 +0100

    Schedule & crosstable improvements

--- src/components/Schedule.tsx
+++ src/components/Schedule.tsx
@@ -2,11 +2,12 @@ import { memo, useEffect, useRef, useState } from "react";
 import type { CCCEngine, CCCEventUpdate, CCCGameUpdate } from "../types";
 import { EngineLogo } from "./EngineLogo";
 import "./Schedule.css";
+import { MdOutlineClose } from "react-icons/md";
 
 type ScheduleProps = {
   engines: CCCEngine[];
   event: CCCEventUpdate;
-  requestEvent: (gameNr: string) => void;
+  requestEvent: (gameNr?: string) => void;
   selectedGame: CCCGameUpdate;
 };
 
@@ -69,66 +70,74 @@ const Schedule = memo(
     ];
 
     return (
-      <div className="schedule" ref={scheduleRef}>
-        {gamesList.map((game, i) => {
-          const gameWhite = engines.find(
-            (engine) => engine.id === game.whiteId
-          )!!;
-          const gameBlack = engines.find(
-            (engine) => engine.id === game.blackId
-          )!!;
+      <>
+        <div className="schedule" ref={scheduleRef}>
+          {gamesList.map((game, i) => {
+            const gameWhite = engines.find(
+              (engine) => engine.id === game.whiteId
+            )!!;
+            const gameBlack = engines.find(
+              (engine) => engine.id === game.blackId
+            )!!;
 
-          const whiteClass =
-            game.outcome === "1-0"
-              ? "winner"
-              : game.outcome === "0-1"
-                ? "loser"
-                : game.timeEnd
-                  ? "draw"
-                  : "tbd";
-          const blackClass =
-            game.outcome === "1-0"
-              ? "loser"
-              : game.outcome === "0-1"
+            const whiteClass =
+              game.outcome === "1-0"
                 ? "winner"
-                : game.timeEnd
-                  ? "draw"
-                  : "tbd";
+                : game.outcome === "0-1"
+                  ? "loser"
+                  : game.timeEnd
+                    ? "draw"
+                    : "tbd";
+            const blackClass =
+              game.outcome === "1-0"
+                ? "loser"
+                : game.outcome === "0-1"
+                  ? "winner"
+                  : game.timeEnd
+                    ? "draw"
+                    : "tbd";
 
-          const isCurrentGame =
-            game.gameNr === event.tournamentDetails.schedule.present?.gameNr;
-          const isSelectedGame =
-            game.gameNr === String(selectedGame.gameDetails.gameNr);
-          const tournamentOver =
-            !event.tournamentDetails.schedule.present &&
-            event.tournamentDetails.schedule.future.length === 0;
-          const gameClass = isCurrentGame || isSelectedGame ? " active" : "";
-          const ref =
-            isCurrentGame || (isSelectedGame && tournamentOver)
-              ? currentGameRef
-              : null;
+            const isCurrentGame =
+              game.gameNr === event.tournamentDetails.schedule.present?.gameNr;
+            const isSelectedGame =
+              game.gameNr === String(selectedGame.gameDetails.gameNr);
+            const tournamentOver =
+              !event.tournamentDetails.schedule.present &&
+              event.tournamentDetails.schedule.future.length === 0;
+            const gameClass = isCurrentGame || isSelectedGame ? " active" : "";
+            const ref =
+              isCurrentGame || (isSelectedGame && tournamentOver)
+                ? currentGameRef
+                : null;
 
-          return (
-            <div
-              className={"game" + gameClass}
-              ref={ref}
-              key={game.gameNr}
-              onClick={() => requestEvent(game.gameNr)}
-            >
-              <span className="round">#{i + 1}</span>
-              <EngineLogo engine={gameWhite} size={28} />
-              <span className={"engineName " + whiteClass}>
-                {gameWhite.name}
-              </span>
-              <span className="vs">vs.</span>
-              <span className={"engineName " + blackClass}>
-                {gameBlack.name}
-              </span>
-              <EngineLogo engine={gameBlack} size={28} />
-            </div>
-          );
-        })}
-      </div>
+            return (
+              <div
+                className={"game" + gameClass}
+                ref={ref}
+                key={game.gameNr}
+                onClick={() => requestEvent(game.gameNr)}
+              >
+                <span className="round">#{i + 1}</span>
+                <EngineLogo engine={gameWhite} size={28} />
+                <span className={"engineName " + whiteClass}>
+                  {gameWhite.name}
+                </span>
+                <span className="vs">vs.</span>
+                <span className={"engineName " + blackClass}>
+                  {gameBlack.name}
+                </span>
+                <EngineLogo engine={gameBlack} size={28} />
+              </div>
+            );
+          })}
+        </div>
+        {String(selectedGame.gameDetails.gameNr) !==
+          event.tournamentDetails.schedule.present?.gameNr && (
+          <button className="closeButton" onClick={() => requestEvent()}>
+            <MdOutlineClose />
+          </button>
+        )}
+      </>
     );
   }
 );
