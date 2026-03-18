d6cddb584605d7a57129eb035a5ef4d72a7ced3b
Yoshie2000 <patrick.leonhardt@gmx.net>
2026-03-18 16:45:14 +0100

    Display non-determinable elo error bars as +/- Inf
    Fixes #32

--- src/components/Popup/Crosstable.tsx
+++ src/components/Popup/Crosstable.tsx
@@ -146,9 +146,10 @@ function calculateEloAndMargin(penta: Penta): {
   const errorMargin = (scoreToElo(maxScore) - scoreToElo(minScore)) / 2;
 
   const eloWithSign = `${elo >= 0 ? "+" : ""}${ELO_FORMATTER.format(elo)}`;
+  const errorMarginString = variance === 0 ? "Inf" : ELO_FORMATTER.format(errorMargin);
 
   return {
-    text: `${eloWithSign} +/- ${ELO_FORMATTER.format(errorMargin)}`,
+    text: `${eloWithSign} +/- ${errorMarginString}`,
     className: elo > 0 ? "win" : elo < 0 ? "loss" : "draw",
   };
 }
