c4f693dbbe48e58dba4ce969e4217850f9affe5b
Yoshie2000 <patrick.leonhardt@gmx.net>
2026-05-19 18:16:45 +0200

    Change source of CCC engine image URL

--- src/components/EngineWindow/EngineLogo.tsx
+++ src/components/EngineWindow/EngineLogo.tsx
@@ -8,11 +8,11 @@ function getImageUrl(
   engine: CCCEngine | undefined,
   provider: "ccc" | "tcec"
 ): string {
-  if (engine?.imageUrl.includes("https")) {
-    return engine.imageUrl;
-  }
-
   if (provider === "tcec") {
+    if (engine?.imageUrl.includes("https")) {
+      return engine.imageUrl;
+    }
+
     if (engine?.imageUrl) {
       const engineName = `${engine.imageUrl[0].toUpperCase()}${engine.imageUrl.slice(1).toLowerCase()}`;
       return `https://ctv.yoshie2000.de/tcec/image/engine/${engineName}.png`;
@@ -21,11 +21,7 @@ function getImageUrl(
     return "https://ctv.yoshie2000.de/tcec/image/tcec2.jpg";
   }
 
-  if (engine?.imageUrl) {
-    return `https://images.chesscomfiles.com/chess-themes/computer_chess_championship/avatars/${engine.imageUrl}.png`;
-  }
-
-  return "https://images.chesscomfiles.com/chess-themes/computer_chess_championship/avatars/heimdall.png";
+  return `https://images.chesscomfiles.com/chess-themes/computer_chess_championship/avatars/${engine?.name.toLowerCase()}.png`;
 }
 
 export function EngineLogo({ engine, size = 36 }: EngineLogoProps) {
@@ -36,7 +32,7 @@ export function EngineLogo({ engine, size = 36 }: EngineLogoProps) {
     <img
       src={src}
       style={{
-        width: `${size * (activeProvider === "tcec" ? 1.5 : 1)}px`,
+        width: `${size * (activeProvider === "tcec" ? 1.75 : 1)}px`,
         height: `${size}px`,
         margin: `${size / 6}px`,
       }}
